86dd667d757ea719f7aae1a77c3cc5f2c444b15e
vineethvijayan <vineeth.vijayan@bs-shipmanagement.com>
2015-03-11 17:08:59 +0200

    first Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,64 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+  temp = $scope.playlists;
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+})
+
+.controller('DashboardCtrl', function($scope, $http) {  
+  $scope.owners = [];
+  $http.get('https://getvesseltracker.com/cwa_mobile_prod/get_user_owners.php?userid=407&appid=156056&pal_user_email=vineeth.vijayan').
+  success(function(data, status, headers, config) {
+    for (var i = data.owners_array.length - 1; i >= 0; i--) {
+      $scope.owners.push({title: data.owners_array[i].label, id: data.owners_array[i].ID});
+    };
+  }).
+  error(function(data, status, headers, config) {
+  });
+});
+
+var temp;
+var temp1;
